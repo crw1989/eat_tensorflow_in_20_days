a224b854dff9696324dfba37311b1586579a3edb
LenovoPC <1162794333@qq.com>
2020-04-05 18:50:47 +0800

    更新了高级API

--- day10.py
+++ day10.py
@@ -11,16 +11,28 @@ input = Input(shape=(2,))
 out = layers.Dense(1)(input)
 model = Model(input, out)
 
-ds = tf.data.Dataset.from_tensor_slices((x, y)).shuffle(buffer_size=1024).batch(64).prefetch(tf.data.experimental.AUTOTUNE)
+ds = tf.data.Dataset.from_tensor_slices((x, y)).shuffle(buffer_size=1024).batch(64).prefetch(
+    tf.data.experimental.AUTOTUNE)
 
-optimizer = tf.optimizers.Adam()
+optimizer = tf.optimizers.Adam(0.01)
+train_loss = tf.metrics.MeanSquaredError('train_loss')
+
+
+@tf.function
+def loss(pred, label):
+    return tf.reduce_mean(tf.losses.mean_squared_error(pred, label))
+
+
+@tf.function
 def train(epoch):
-    for i in range(epoch):
+    for i in tf.range(epoch):
         for x_batch, y_batch in ds:
             with tf.GradientTape() as tape:
                 y_pred = model(x_batch)
-                loss = tf.reduce_mean(tf.losses.mean_squared_error(y_batch, y_pred))
-            gradient = tape.gradient(loss,model.variables)
-            optimizer.apply_gradients(zip(gradient,model.variables))
-        tf.print('epoch:',i,',loss:',loss)
-train(1000)
+                loss_val = loss(y_pred, y_batch)
+            train_loss.update_state(loss_val,y_batch)
+            gradient = tape.gradient(loss_val, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradient, model.trainable_variables))
+        tf.print('epoch:', i, 'loss:', train_loss.result())
+
+train(10000)
